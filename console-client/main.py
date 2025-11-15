00f136ab6bb2b34b6707b9ebf82570986cd03b86
DeboraOnGit <s3532208@vuw.leidenuniv.nl>
2025-11-15 10:37:33 +0100

    Login ui and 2fa

---
+++ console-client/main.py
@@ -0,0 +1,108 @@
+import requests
+import getpass
+
+API_URL = "http://localhost:5000"  # Gateway URL
+AUTH_URL = "http://localhost:5001" # Authetication URL
+
+
+def login():
+    while True:
+        print("Welcome to the translation app")
+        username = input("Username: ")
+        password = getpass.getpass("Password: ")
+
+        response = requests.post(f"{API_URL}/login", json={
+            "username": username,
+            "password": password
+        })
+
+        if response.status_code == 200:
+            # Handle 2fa
+            if response.json().get("2fa_required"):
+                print("2FA required. Please enter your 6-digit code.")
+                totp_code = input("TOTP code: ")
+                totp_response = requests.post(f"{AUTH_URL}/login/totp", json={"user_id": response.json()["user_id"], "totp_code": totp_code})
+
+                if totp_response.status_code == 200:
+                    print(totp_response.json()["message"])
+                    token = totp_response.json()["access_token"]
+                    return token
+                else:
+                    print("Login failed due to wrong totp code")
+                    continue
+            else:
+                print(response.json()["message"])
+                token = response.json()["access_token"]
+                return token
+        else:
+            print(response.json()["message"])
+            continue
+
+def translate(token):
+    text = input("Text to translate: ")
+    # TODO: Select target language
+    # print("\n Select target language")
+    # print("1. Bulgarian")
+    # print("2. Dutch")
+    # target_lang = input("Select option: ")
+
+    headers = {"Authorization": f"Bearer {token}"}
+    response = requests.post(f"{API_URL}/translate", json={
+        "text": text,
+        #"target_lang": target_lang
+    }, headers=headers)
+
+    if response.status_code == 200:
+        print("Translation result:", response.json().get("translated_text"))
+    else:
+        print("Translation failed:", response.json().get("message"))
+
+def enable_2fa(token):
+    headers = {"Authorization": f"Bearer {token}"}
+    response = requests.post(f"{AUTH_URL}/enable-2fa", headers=headers)
+
+    # If backend returns JSON with TOTP info:
+    if response.status_code == 200:
+        data = response.json()
+        # print("Message:", data.get("message"))
+        print("Provisioning URI:", data.get("qr_uri"))
+
+def logout(token):
+    headers = {"Authorization": f"Bearer {token}"}
+    response = requests.post(f"{AUTH_URL}/logout", headers=headers)
+    if response.status_code == 200:
+        print(response.json().get("messg"))
+        return None
+    else:
+        print("Logout failed:", response.text)
+        return token
+
+def main_menu(token):
+    while True:
+        print("\n--- Main Menu ---")
+        print("1. Translate text")
+        print("2. Enable  2FA")
+        print("3. Logout")
+        choice = input("Select option: ")
+
+        if choice == "1":
+            translate(token)
+        elif choice == "2":
+            enable_2fa(token)
+        elif choice == "3":
+            token = logout(token)
+            if token:
+                main_menu(token)
+            else:
+                # After logout, new login is possible
+                token = login()
+                if token:
+                    main_menu(token)
+                break
+        else:
+            print("Invalid choice.")
+
+if __name__ == "__main__":
+    token = login()
+    if token:
+        main_menu(token)
